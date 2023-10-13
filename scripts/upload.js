29a74e1cd5b649c8a5ca1221ef79f2f55fee38f7
Tomáš <tomas.kubicek.022@pslib.cz>
2023-10-13 17:50:33 +0200

    new design, files, output, dropdowns, unfinished

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,82 +1,164 @@
-let state = 0
+const path = require('path');
+
 var imageData = []
+let files = []
+let currentFilePage = 0
+let pageCount = 0
 
 const updateFiles = (e) => {
+    fileInputs.forEach(input => {
+
+        //check if file is already in array
+        let inArr = false
+        for (let file of files) {
+            for (let inputfile of input.files) {
+                if (file.path == inputfile.path) {
+                    inArr = true
+                }
+            }
+        }
+        console.log(input.files);
+        if (!inArr) files.push(...input.files)
+
+        for (let file of input.files) {
+            console.log(file);
+            let reader = new FileReader()
+            reader.onload = () => {
+                console.log(reader);
+                imageData.push({ data: file.path, name: file.name })
+                let img = new Image();
+                let objectUrl = window.URL.createObjectURL(file);
+                img.onload = function() {
+                    fileSlider.insertAdjacentHTML("beforeend",
+                        `<div class="file-container">
+                        <button class="file-remove" onclick="removeFile(event)" data-path="${file.path}"><svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 100 80" x="0px" y="0px"><path data-name="Path" d="M39.1,10.8a4,4,0,0,0,0,8H60.9a4,4,0,1,0,0-8Z"></path><path data-name="Compound Path" d="M83.4,22.4H16.6a4,4,0,0,0,0,8h4l4.3,47.8a12,12,0,0,0,12,11H63.1a12,12,0,0,0,12-11l4.3-47.8h4a4,4,0,0,0,0-8ZM67.1,77.5a4,4,0,0,1-4,3.7H36.9a4,4,0,0,1-4-3.7L28.7,30.4H71.3Z"></path></svg></button>
+                        ${img.outerHTML}
+                        <div class="file-info">
+                            <h3 class="file-name">${file.name.split(".").slice(0,-1).join(".").slice(0,18)}</h3>
+                            <h4 class="file-path">${path.dirname(file.path).slice(0,36)}</h4>
+                            <div class="file-stats">
+                                <span class="file-size">${humanFileSize(file.size)}</span>
+                                <span class="file-res">${this.width + "×" + this.height}</span>
+                                <span class="file-type">${file.type.split("/")[1]}</span>
+                            </div>
+                        </div>
+                    </div>`
+                    )
+                }
+                img.src = objectUrl;
+                img.classList.add("file-image")
+            }
+            reader.readAsDataURL(file)
+        }
+    })
     imageData = []
-    const files = e.target.files
     console.log(files);
 
-    if (files.length === 1) {
-        var fileName = files[0].name.split('\\').pop();
-        textContainer.innerHTML = fileName;
-    } else {
-        textContainer.innerHTML = files.length + ' souborů vybráno';
-    }
+    updateFileView()
 
-    infobox.innerHTML = ""
+}
+const fileSlider = document.querySelector("#files-slider")
+const fileArrowLeft = document.querySelector("#files-arrow-left")
+const fileArrowRight = document.querySelector("#files-arrow-right")
+const fileSliderPages = document.querySelector(".files-slider-pages")
+const fileCount = document.querySelector("#files-counter")
+const fileInputs = document.querySelectorAll(".file-input")
+fileInputs.forEach(input => {
+    input.addEventListener('change', updateFiles)
+})
 
-    for (i in files) {
-        let file = files[i]
-        console.log(file);
-        let reader = new FileReader()
-        reader.onload = () => {
-            console.log(reader);
-            imageData.push({ data: file.path, name: file.name })
-            infobox.innerHTML += `<div class="imageinfo">
-      <img class="imagepreview" src="${reader.result}">
-      <div class="imagestats">
-      <span class="imagename">${file.name.slice(0,24)}</span>
-      <span class="imagetype">${file.size} b</span>
-      <span class="imagesize">${file.type}</span>
-      </div>
-    </div>`
+// remove file from array and dom
+const removeFile = (e) => {
+    const path = e.target.dataset.path
+    console.log(path);
+    for (let i = 0; i < files.length; i++) {
+        if (files[i].path == path) {
+            files.splice(i, 1)
+            break
         }
-        reader.readAsDataURL(file)
     }
-
-}
-const fileInput = document.querySelector("#file-input")
-const textContainer = document.querySelector(".file-msg");
-const infobox = document.querySelector(".info");
-console.log(fileInput);
-fileInput.onchange = updateFiles
-
-const content = {
-    files: document.querySelector("#content-files"),
-    settings: document.querySelector("#content-settings"),
-    export: document.querySelector("#content-export"),
+    e.target.parentElement.remove()
+    updateFileView()
 }
 
 
-const nextBtn = document.querySelector("#next")
-nextBtn.onclick = () => {
-    state++
-    setState()
-}
-const backBtn = document.querySelector("#back")
-backBtn.onclick = () => {
-    state--
-    setState()
-}
-
-function setState() {
-    if (state < 0) state = 0
-    if (state > 2) state = 2
-    if (state == 0) {
-        backBtn.setAttribute("disabled", "")
-        content.files.classList.add("visible")
-        content.settings.classList.remove("visible")
-        content.export.classList.remove("visible")
+// convert bytes to human readable file size
+const humanFileSize = (bytes, si = false, dp = 1) => {
+    const thresh = 1000;
+    if (Math.abs(bytes) < thresh) {
+        return bytes + ' B';
     }
-    if (state == 1) {
-        backBtn.removeAttribute("disabled")
-        content.files.classList.remove("visible")
-        content.settings.classList.add("visible")
-        content.export.classList.remove("visible")
+    const units = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    let u = -1;
+    const r = 10 ** dp;
+    do {
+        bytes /= thresh;
+        u++;
+    } while (
+        Math.round(Math.abs(bytes) * r) / r >= thresh &&
+        u < units.length - 1
+    );
+    return bytes.toFixed(dp) + ' ' + units[u];
+}
+
+const updateFileView = () => {
+    //update file count, slider, arrows and file array
+    fileCount.innerText = files.length
+    pageCount = Math.ceil((files.length + 1) / 4)
+
+    //update arrows
+    if (currentFilePage >= pageCount) currentFilePage = pageCount - 1
+    updateArrows()
+
+    fileSliderPages.innerHTML = ""
+        //update slider dots
+    for (let i = 0; i < pageCount; i++) {
+        fileSliderPages.insertAdjacentHTML("beforeend", `<div class="files-slider-page" onclick="changePage(event)"></div>`)
     }
-    if (state == 2) {
-        content.files.classList.remove("visible")
-        content.settings.classList.remove("visible")
-        content.export.classList.add("visible")
+    fileSliderPages.children[currentFilePage].classList.add("active")
+
+}
+
+const updateArrows = () => {
+    if (currentFilePage < pageCount - 1) {
+        fileArrowRight.classList.remove("disabled")
+    } else {
+        fileArrowRight.classList.add("disabled")
+    }
+    if (currentFilePage >= 1) {
+        fileArrowLeft.classList.remove("disabled")
+    } else {
+        fileArrowLeft.classList.add("disabled")
     }
 }
+
+const changePage = (e) => {
+    currentFilePage = [...fileSliderPages.children].indexOf(e.target)
+    fileSlider.scrollLeft = (fileSlider.clientWidth + 16) * currentFilePage
+    updateArrows()
+        //update slider dots
+    fileSliderPages.querySelector(".active").classList.remove("active")
+    fileSliderPages.children[currentFilePage].classList.add("active")
+}
+
+fileArrowLeft.addEventListener("click", () => {
+    if (currentFilePage >= 1) {
+        currentFilePage--
+        fileSlider.scrollLeft = (fileSlider.clientWidth + 16) * currentFilePage
+        updateArrows()
+            //update slider dots
+        fileSliderPages.children[currentFilePage].classList.add("active")
+        fileSliderPages.children[currentFilePage + 1].classList.remove("active")
+    }
+})
+
+fileArrowRight.addEventListener("click", () => {
+    if (currentFilePage < pageCount - 1) {
+        currentFilePage++
+        fileSlider.scrollLeft = (fileSlider.clientWidth + 16) * currentFilePage
+        updateArrows()
+            //update slider dots
+        fileSliderPages.children[currentFilePage].classList.add("active")
+        fileSliderPages.children[currentFilePage - 1].classList.remove("active")
+    }
+})
